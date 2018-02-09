e6ebf31ee4218aa207503be8e74b57aedb7a7a17
Alexander Lakhin <exclusion@gmail.com>
2018-02-09 07:05:18 +0300

    Improve create-test-user.bat

--- windows/scripts/create-test-user.bat
+++ windows/scripts/create-test-user.bat
@@ -1,16 +1,13 @@
 REM Disable PasswordComplexity requirement
 secedit /export /cfg %TEMP%\secpol.cfg
-cmd /c powershell -Command "(Get-Content %TEMP%\secpol.cfg) | ForEach-Object {$_ -replace \"PasswordComplexity = 1\", \"PasswordComplexity = 0\"} | Set-Content %TEMP%\secpol.cfg"
+powershell -Command "(Get-Content %TEMP%\secpol.cfg) | ForEach-Object {$_ -replace \"PasswordComplexity = 1\", \"PasswordComplexity = 0\"} | Set-Content %TEMP%\secpol.cfg"
 secedit /configure /db %windir%\security\local.sdb /cfg %TEMP%\secpol.cfg /areas SECURITYPOLICY
 
 REM Create user
 net user test TestPass1 /add
-Set AdmGroupSID=S-1-5-32-544
-Set AdmGroup=
-For /F "UseBackQ Tokens=1* Delims==" %%I In (`WMIC Group Where "SID = '%AdmGroupSID%'" Get Name /Value ^| Find "="`) Do Set AdmGroup=%%J
-Set AdmGroup=%AdmGroup:~0,-1%
-net localgroup %AdmGroup% test /add
-wmic USERACCOUNT WHERE "Name='test'" set PasswordExpires=FALSE
+powershell -Command "$admgroup=(New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-544\")).Translate([System.Security.Principal.NTAccount]).Value.Split(\"\\\")[1]; net localgroup $admgroup test /add"
+WMIC UserAccount WHERE "Name='test'" SET PasswordExpires=FALSE
+net user test
 
 REM Create user profile
 powershell -Command "Start-Process whoami -LoadUserProfile -Credential (new-object -typename System.Management.Automation.PSCredential -argumentlist \"test\", (convertto-securestring \"TestPass1\" -asplaintext -force))"
