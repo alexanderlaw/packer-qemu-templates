b28d15754450f3b41d13270051e9ef6799d117e5
Alexander Lakhin <exclusion@gmail.com>
2018-02-09 07:08:15 +0300

    Add Windows 7 SP1 (32- and 64-bit)

---
+++ windows/floppy/windows-7sp1-enterprise-32/configure2.bat
@@ -0,0 +1,23 @@
+:: Download and install .Net 4.0
+powershell -Command "((new-object net.webclient).DownloadFile('https://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe', '%TEMP%\dotNetFx40_Full_x86_x64.exe'))
+%TEMP%\dotNetFx40_Full_x86_x64.exe /passive /norestart
+
+:: Download and install PowerShell 3
+powershell -Command "((new-object net.webclient).DownloadFile('http://download.microsoft.com/download/E/7/6/E76850B8-DA6E-4FF5-8CCE-A24FC513FD16/Windows6.1-KB2506143-x86.msu', '%TEMP%\Windows6.1-KB2506143-x86.msu'))
+wusa.exe %TEMP%\Windows6.1-KB2506143-x86.msu /quiet /norestart
+
+:: Download and install HotFix for KB2842230
+powershell -Command "((new-object net.webclient).DownloadFile('http://hotfixv4.microsoft.com/Windows%%207/Windows%%20Server2008%%20R2%%20SP1/sp2/Fix467402/7600/free/463983_intl_i386_zip.exe', '%TEMP%\463983_intl_i386.zip'))
+powershell -Command "$shell = New-Object -ComObject Shell.Application; $zip_src = $shell.NameSpace('%TEMP%\463983_intl_i386.zip'); $zip_dest = $shell.NameSpace('%TEMP%'); $zip_dest.CopyHere($zip_src.Items(), 1044)"
+wusa.exe %TEMP%\Windows6.1-KB2842230-x86.msu /quiet /norestart
+
+:: Remove temporary files
+del %TEMP%\*.exe
+del %TEMP%\*.msu
+
+:: start winrm
+cmd /c a:\install-winrm.cmd
+
+cmd /c a:\zz-start-sshd.cmd
+
+del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\configure2.bat"
---
+++ windows/floppy/windows-7sp1-enterprise/configure.bat
@@ -0,0 +1,11 @@
+:: %~dp0 gives the dirname of the script
+
+:: Download Service Pack 1
+powershell -Command "((new-object net.webclient).DownloadFile('https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe', '%TEMP%\windows6.1-KB976932-X64.exe'))
+
+:: Setup the script to run after the Service Pack installation
+mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy %~dp0configure2.bat "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+:: Install Service Pack 1
+%TEMP%\windows6.1-KB976932-X64.exe /unattend
